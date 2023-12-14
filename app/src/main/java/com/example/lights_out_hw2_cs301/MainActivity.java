42b11103076feba1ac1817953358d51d29f7d60c
oliviasendelbach <sendelba26@up.edu>
2023-12-14 12:51:47 -0800

    Declared buttons and created board. I also implemented the onClick listener so I could make the method.

--- app/src/main/java/com/example/lights_out_hw2_cs301/MainActivity.java
+++ app/src/main/java/com/example/lights_out_hw2_cs301/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.lights_out_hw2_cs301;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        private Button button0 = null;
+        private Button button1 = null;
+        private Button button2 = null;
+        private Button button3 = null;
+        private Button button4 = null;
+        private Button button5 = null;
+        private Button button6 = null;
+        private Button button7 = null;
+        private Button button8 = null;
+        private Button button9 = null;
+        private Button button10 = null;
+        private Button button11 = null;
+        private Button button12 = null;
+        private Button button13 = null;
+        private Button button14 = null;
+        private Button button15 = null;
+        private Button button16 = null;
+        private Button button17 = null;
+        private Button button18 = null;
+        private Button button19 = null;
+        private Button button20 = null;
+        private Button button21 = null;
+        private Button button22 = null;
+        private Button button23 = null;
+        private Button button24 = null;
+
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
